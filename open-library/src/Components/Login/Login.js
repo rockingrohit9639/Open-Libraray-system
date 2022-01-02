5a5fcd2fb01353904d964afd7550246f9172e550
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-01-02 20:10:57 +0530

    added login form

--- open-library/src/Components/Login/Login.js
+++ open-library/src/Components/Login/Login.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 function Login() {
-    return (
-        <div>
-            
+  return (
+    <div className="login">
+      <form className="login_container">
+        <div class="mb-3">
+          <label for="email" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="name@example.com"
+          />
         </div>
-    )
+        <div class="mb-3">
+          <label for="password" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            name="password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
